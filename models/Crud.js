dff8d452125813d7411fba33ff5584c2f374af12
daniyal <daniyalnaeem5@gmail.com>
2020-10-27 11:31:54 +0500

    create room

---
+++ models/Crud.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const {check, validationResult} = require('express-validator');
+const auth = require("../middleware/auth");
+const User = require('../models/User');
+const Crud = require('../models/Crud');
+
+const CrudSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  classname: {
+    type: String,
+    required: true
+  },
+  classid: {
+    type: String,
+    required: true
+  },
+ 
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('crud', CrudSchema);
--- routes/crud.js
+++ routes/crud.js
@@ -1,38 +1,123 @@
 const express = require('express');
 const router = express.Router();
-
+const {check, validationResult} = require('express-validator');
+const auth = require("../middleware/auth");
+const User = require('../models/User');
+const Crud = require('../models/Crud');
 
 // @route     GET api/crud
 // @desc      create all meeting rooms
 // @access    Private
-router.get("/", (req, res) => {
+router.get("/", auth , async (req, res) => {
 
-    res.send('Get all Meeting Room');
+    try {
+        const crud = await Crud.find({user: req.user.id}).sort({
+          date: -1,
+        });
+        res.json(crud);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
 });
 
 // @route     POST api/crud
 // @desc      add all meeting rooms
 // @access    Private
-router.post("/", (req, res) => {
+router.post(
+    '/',
+    [
+      auth,
+      [
+        check('classname', 'ClassName is required')
+          .not()
+          .isEmpty(),
+      ],
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+      }
+  
+      const {classname, classid, type} = req.body;
+  
+      try {
+        const newCrud = new Crud({
+          classname,
+          classid,
+          type,
+        
+          user: req.user.id,
+        });
+  
+        const crud = await newCrud.save();
+  
+        res.json(crud);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
+    },
+  );
 
-    res.send('Add new Meeting Room');
-});
-
-// @route     PUT api/crud
-// @desc      update all meeting rooms
+// @route     PUT api/crud/:id
+// @desc      Update crud
 // @access    Private
-router.put("/:id", (req, res) => {
+router.put('/:id', auth, async (req, res) => {
+  const  {classname, classid, type} = req.body;
 
-    res.send('Update Meeting Room');
+  // Build crud object
+  const crudFields = {};
+  if (classname) crudFields.classname = classname;
+  if (classid) crudFields.classid = classid;
+  if (type) crudFields.type = type;
+
+  try {
+    let crud = await Crud.findById(req.params.id);
+
+    if (!crud) return res.status(404).json({msg: 'crud not found'});
+
+    // Make sure user owns crud
+    if (crud.user.toString() !== req.user.id) {
+      return res.status(401).json({msg: 'Not authorized'});
+    }
+
+    crud = await Crud.findByIdAndUpdate(
+      req.params.id,
+      {$set: crudFields},
+      {new: true},
+    );
+
+    res.json(crud);
+  } catch (err) {
+    console.error(er.message);
+    res.status(500).send('Server Error');
+  }
 });
 
-// @route     DELETE api/crud
-// @desc      delete meeting rooms
+// @route     DELETE api/crud/:id
+// @desc      Delete crud
 // @access    Private
-router.delete("/:id", (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let crud = await Crud.findById(req.params.id);
 
-    res.send('Delete Meeting Room');
+    if (!crud) return res.status(404).json({msg: 'crud not found'});
+
+    // Make sure user owns crud
+    if (crud.user.toString() !== req.user.id) {
+      return res.status(401).json({msg: 'Not authorized'});
+    }
+
+    await 
+    Crud.findByIdAndRemove(req.params.id);
+
+    res.json({msg: 'crud removed'});
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
-
 
 module.exports = router;
