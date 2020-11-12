3c5e9c9486eb159801d47fa315408548441307c7
daniyal <daniyalnaeem5@gmail.com>
2020-11-12 14:06:25 +0500

    port

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/app',
-  port: '3030'
+  port: '443'
 }) 
 
 let myVideoStream;
